430d1c9745b9a91c4345348c237ea9b865ad0bfa
LAM HENG YEE RP <20014013@myrp.edu.sg>
2022-06-08 18:00:21 +0800

    change some of the line by hy

--- lib/Journey/journey.dart
+++ lib/Journey/journey.dart
@@ -35,6 +35,7 @@ class _HomePageWidgetState extends State<HomePageWidget> {
       FirebaseFirestore.instance.collection('Account').snapshots();
 
   int _selectedIndex = 0;
+  // ignore: unused_field
   static const List<Widget> _widgetOptions = <Widget>[];
 
   void _onItemTapped(int index) {

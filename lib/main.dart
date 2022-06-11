961ce0a749a3bfb6f1b0a7c50c6fd7cc9a2605a5
LAM HENG YEE RP <20014013@myrp.edu.sg>
2022-06-11 12:23:00 +0800

    chnage main.dart by hy

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,7 @@ void main() async {
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key key}) : super(key: key);
+  const MyApp({Key? key}) : super(key: key);
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {

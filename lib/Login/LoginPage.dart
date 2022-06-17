e6b00fd5fe85a956b0a11419e9abd9840424fc9d
LAM HENG YEE RP <20014013@myrp.edu.sg>
2022-06-17 18:43:52 +0800

    reformat loginPage code by hy

--- lib/Login/LoginPage.dart
+++ lib/Login/LoginPage.dart
@@ -38,6 +38,184 @@ class _LoginPageWidgetState extends State<LoginPageWidget> {
 
   @override
   Widget build(BuildContext context) {
+    //Email Address text field
+    final emailAddress = Padding(
+      padding: EdgeInsetsDirectional.fromSTEB(20, 20, 20, 0),
+      child: Row(
+        mainAxisSize: MainAxisSize.max,
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Expanded(
+            child: TextFormField(
+              controller: emailController,
+              autofocus: false,
+              obscureText: false,
+              keyboardType: TextInputType.emailAddress,
+              validator: (value) {
+                if (value!.isEmpty) {
+                  return ("Please enter your email");
+                }
+                // reg expression for email validation
+                if (!RegExp("^[a-zA-Z0-9+_.-]+@[a-zA-Z0-9+_.-]+.[a-z]")
+                    .hasMatch(value)) {
+                  return ("Please Enter a valid email");
+                }
+                return null;
+              },
+              onSaved: (value) {
+                emailController.text = value!;
+              },
+              textInputAction: TextInputAction.next,
+              decoration: InputDecoration(
+                labelText: 'Email Address',
+                enabledBorder: OutlineInputBorder(
+                  borderSide: BorderSide(
+                    color: Color(0xFFBDBDBDBD),
+                    width: 1,
+                  ),
+                  borderRadius: BorderRadius.circular(8),
+                ),
+                focusedBorder: OutlineInputBorder(
+                  borderSide: BorderSide(
+                    color: Color(0xFFBDBDBDBD),
+                    width: 1,
+                  ),
+                  borderRadius: BorderRadius.circular(8),
+                ),
+              ),
+              style: TextStyle(fontFamily: "Poppins"),
+            ),
+          ),
+        ],
+      ),
+    );
+
+    //Password text field
+    final passwordUser = Padding(
+      padding: EdgeInsetsDirectional.fromSTEB(20, 16, 20, 0),
+      child: Row(
+        mainAxisSize: MainAxisSize.max,
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Expanded(
+            child: TextFormField(
+              controller: passwordController,
+              // onChanged: (_) => EasyDebounce.debounce(
+              //   'textController2',
+              //   Duration(milliseconds: 2000),
+              //   () => setState(() {}),
+              // ),
+              autofocus: false,
+              obscureText: !passwordVisibility1,
+              // ignore: missing_return, body_might_complete_normally_nullable
+              validator: (value) {
+                RegExp regEx = RegExp(r'^.{7,}$');
+                if (value!.isEmpty) {
+                  return ("Please enter password");
+                }
+
+                if (!regEx.hasMatch(value)) {
+                  return ("Please enter valid password(Min. 7 character)");
+                }
+              },
+              onSaved: (value) {
+                passwordController.text = value!;
+              },
+              textInputAction: TextInputAction.done,
+              decoration: InputDecoration(
+                labelText: 'Password',
+                enabledBorder: OutlineInputBorder(
+                  borderSide: BorderSide(
+                    color: Color(0xFFBDBDBDBD),
+                    width: 1,
+                  ),
+                  borderRadius: BorderRadius.circular(8),
+                ),
+                focusedBorder: OutlineInputBorder(
+                  borderSide: BorderSide(
+                    color: Color(0xFFBDBDBDBD),
+                    width: 1,
+                  ),
+                  borderRadius: BorderRadius.circular(8),
+                ),
+                suffixIcon: InkWell(
+                  onTap: () => setState(
+                    () => passwordVisibility1 = !passwordVisibility1,
+                  ),
+                  focusNode: FocusNode(skipTraversal: true),
+                  child: Icon(
+                    passwordVisibility1
+                        ? Icons.visibility_outlined
+                        : Icons.visibility_off_outlined,
+                    color: Color(0xFF757575),
+                    size: 22,
+                  ),
+                ),
+              ),
+              style: TextStyle(fontFamily: "Poppins"),
+            ),
+          ),
+        ],
+      ),
+    );
+
+    //forget password button
+    final forgetPasswordBtt = TextButton(
+      onPressed: () {},
+      child: Text(
+        "Forgot password?",
+        style: TextStyle(
+            fontFamily: 'Lexend Deca',
+            fontSize: 12,
+            fontWeight: FontWeight.w500,
+            color: Color(0xFF000000)),
+      ),
+      style: TextButton.styleFrom(
+        padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 5),
+        primary: Color(0xFFF1FDFB),
+      ),
+    );
+
+    //forget password button
+    final loginBtt = ElevatedButton(
+      onPressed: () {
+        signIn((emailController.text), passwordController.text);
+      },
+      child: const Text(
+        "LOGIN",
+        style: TextStyle(
+          fontFamily: 'Lexend Deca',
+          fontSize: 14,
+          fontWeight: FontWeight.bold,
+        ),
+      ),
+      style: ElevatedButton.styleFrom(
+        padding: const EdgeInsets.symmetric(horizontal: 30, vertical: 10),
+        primary: Color(0xFF00e5FF),
+      ),
+    );
+
+    //create account button
+    final createAccountBtt = TextButton(
+      onPressed: () {
+        Navigator.push(
+          context,
+          MaterialPageRoute(builder: (context) => SignUpPageWidget()),
+        );
+      },
+      child: const Text(
+        "Create Account",
+        style: TextStyle(
+            fontFamily: 'Poppins',
+            fontSize: 12,
+            fontWeight: FontWeight.w500,
+            color: Color(0xFF212121)),
+      ),
+      style: TextButton.styleFrom(
+          padding: const EdgeInsets.symmetric(horizontal: 5, vertical: 10),
+          primary: Color(0xFFF1FDFB)),
+    );
+
     return Scaffold(
       key: scaffoldKey,
       backgroundColor: Color(0xFFF1FDFB),
@@ -91,197 +269,25 @@ class _LoginPageWidgetState extends State<LoginPageWidget> {
                             ),
                           ],
                         ),
-                        Padding(
-                          padding:
-                              EdgeInsetsDirectional.fromSTEB(20, 20, 20, 0),
-                          child: Row(
-                            mainAxisSize: MainAxisSize.max,
-                            mainAxisAlignment: MainAxisAlignment.center,
-                            children: [
-                              Expanded(
-                                child: TextFormField(
-                                  controller: emailController,
-                                  // onChanged: (_) => EasyDebounce.debounce(
-                                  //   'textController1',
-                                  //   Duration(milliseconds: 2000),
-                                  //   () => setState(() {}),
-                                  // ),
-                                  autofocus: false,
-                                  obscureText: false,
-                                  keyboardType: TextInputType.emailAddress,
-                                  validator: (value) {
-                                    if (value!.isEmpty) {
-                                      return ("Please enter your email");
-                                    }
-                                    // reg expression for email validation
-                                    if (!RegExp(
-                                            "^[a-zA-Z0-9+_.-]+@[a-zA-Z0-9+_.-]+.[a-z]")
-                                        .hasMatch(value)) {
-                                      return ("Please Enter a valid email");
-                                    }
-                                    return null;
-                                  },
-                                  onSaved: (value) {
-                                    emailController.text = value!;
-                                  },
-                                  textInputAction: TextInputAction.next,
-                                  decoration: InputDecoration(
-                                    labelText: 'Email Address',
-                                    enabledBorder: OutlineInputBorder(
-                                      borderSide: BorderSide(
-                                        color: Color(0xFFBDBDBDBD),
-                                        width: 1,
-                                      ),
-                                      borderRadius: BorderRadius.circular(8),
-                                    ),
-                                    focusedBorder: OutlineInputBorder(
-                                      borderSide: BorderSide(
-                                        color: Color(0xFFBDBDBDBD),
-                                        width: 1,
-                                      ),
-                                      borderRadius: BorderRadius.circular(8),
-                                    ),
-                                  ),
-                                  style: TextStyle(fontFamily: "Poppins"),
-                                ),
-                              ),
-                            ],
-                          ),
-                        ),
-                        Padding(
-                          padding:
-                              EdgeInsetsDirectional.fromSTEB(20, 16, 20, 0),
-                          child: Row(
-                            mainAxisSize: MainAxisSize.max,
-                            mainAxisAlignment: MainAxisAlignment.center,
-                            children: [
-                              Expanded(
-                                child: TextFormField(
-                                  controller: passwordController,
-                                  // onChanged: (_) => EasyDebounce.debounce(
-                                  //   'textController2',
-                                  //   Duration(milliseconds: 2000),
-                                  //   () => setState(() {}),
-                                  // ),
-                                  autofocus: false,
-                                  obscureText: !passwordVisibility1,
-                                  // ignore: missing_return, body_might_complete_normally_nullable
-                                  validator: (value) {
-                                    RegExp regEx = RegExp(r'^.{7,}$');
-                                    if (value!.isEmpty) {
-                                      return ("Please enter password");
-                                    }
-
-                                    if (!regEx.hasMatch(value)) {
-                                      return ("Please enter valid password(Min. 7 character)");
-                                    }
-                                  },
-                                  onSaved: (value) {
-                                    passwordController.text = value!;
-                                  },
-                                  textInputAction: TextInputAction.done,
-                                  decoration: InputDecoration(
-                                    labelText: 'Password',
-                                    enabledBorder: OutlineInputBorder(
-                                      borderSide: BorderSide(
-                                        color: Color(0xFFBDBDBDBD),
-                                        width: 1,
-                                      ),
-                                      borderRadius: BorderRadius.circular(8),
-                                    ),
-                                    focusedBorder: OutlineInputBorder(
-                                      borderSide: BorderSide(
-                                        color: Color(0xFFBDBDBDBD),
-                                        width: 1,
-                                      ),
-                                      borderRadius: BorderRadius.circular(8),
-                                    ),
-                                    suffixIcon: InkWell(
-                                      onTap: () => setState(
-                                        () => passwordVisibility1 =
-                                            !passwordVisibility1,
-                                      ),
-                                      focusNode: FocusNode(skipTraversal: true),
-                                      child: Icon(
-                                        passwordVisibility1
-                                            ? Icons.visibility_outlined
-                                            : Icons.visibility_off_outlined,
-                                        color: Color(0xFF757575),
-                                        size: 22,
-                                      ),
-                                    ),
-                                  ),
-                                  style: TextStyle(fontFamily: "Poppins"),
-                                ),
-                              ),
-                            ],
-                          ),
-                        ),
+                        //Email Address text field
+                        emailAddress,
+                        //Password text field
+                        passwordUser,
                         Padding(
                           padding: EdgeInsetsDirectional.fromSTEB(5, 10, 20, 0),
                           child: Row(
                             mainAxisSize: MainAxisSize.max,
                             mainAxisAlignment: MainAxisAlignment.spaceBetween,
                             children: [
-                              TextButton(
-                                onPressed: () {},
-                                child: Text(
-                                  "Forgot password?",
-                                  style: TextStyle(
-                                      fontFamily: 'Lexend Deca',
-                                      fontSize: 12,
-                                      fontWeight: FontWeight.w500,
-                                      color: Color(0xFF000000)),
-                                ),
-                                style: TextButton.styleFrom(
-                                  padding: const EdgeInsets.symmetric(
-                                      horizontal: 20, vertical: 5),
-                                  primary: Color(0xFFF1FDFB),
-                                ),
-                              ),
-                              ElevatedButton(
-                                onPressed: () {
-                                  signIn((emailController.text),
-                                      passwordController.text);
-                                },
-                                child: const Text(
-                                  "LOGIN",
-                                  style: TextStyle(
-                                    fontFamily: 'Lexend Deca',
-                                    fontSize: 14,
-                                    fontWeight: FontWeight.bold,
-                                  ),
-                                ),
-                                style: ElevatedButton.styleFrom(
-                                  padding: const EdgeInsets.symmetric(
-                                      horizontal: 30, vertical: 10),
-                                  primary: Color(0xFF00e5FF),
-                                ),
-                              ),
+                              //forget password button
+                              forgetPasswordBtt,
+                              //login button
+                              loginBtt,
                             ],
                           ),
                         ),
-                        TextButton(
-                          onPressed: () {
-                            Navigator.push(
-                              context,
-                              MaterialPageRoute(
-                                  builder: (context) => SignUpPageWidget()),
-                            );
-                          },
-                          child: const Text(
-                            "Create Account",
-                            style: TextStyle(
-                                fontFamily: 'Poppins',
-                                fontSize: 12,
-                                fontWeight: FontWeight.w500,
-                                color: Color(0xFF212121)),
-                          ),
-                          style: TextButton.styleFrom(
-                              padding: const EdgeInsets.symmetric(
-                                  horizontal: 5, vertical: 10),
-                              primary: Color(0xFFF1FDFB)),
-                        ),
+                        //create account button
+                        createAccountBtt,
                       ],
                     ),
                   ),
@@ -301,8 +307,8 @@ class _LoginPageWidgetState extends State<LoginPageWidget> {
           .signInWithEmailAndPassword(email: email, password: password)
           .then((uid) => {
                 Fluttertoast.showToast(msg: "Login Successful"),
-                Navigator.of(context).pushReplacement(MaterialPageRoute(
-                    builder: (context) => HomePageWidget()))
+                Navigator.of(context).pushReplacement(
+                    MaterialPageRoute(builder: (context) => HomePageWidget()))
               })
           .catchError((e) {
         Fluttertoast.showToast(msg: e!.message);

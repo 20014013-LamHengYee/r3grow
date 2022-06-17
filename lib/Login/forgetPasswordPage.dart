6873beb44dbd5b18be76bf2057b13e09459fa987
LAM HENG YEE RP <20014013@myrp.edu.sg>
2022-06-17 21:18:48 +0800

    forgetPassword reformatted UI by hy

--- lib/Login/forgetPasswordPage.dart
+++ lib/Login/forgetPasswordPage.dart
@@ -21,6 +21,67 @@ class _ForgetPasswordPageWidgetState extends State<ForgetPasswordPageWidget> {
 
   @override
   Widget build(BuildContext context) {
+    //email address text field
+    final emailAddress = Expanded(
+      child: Align(
+        alignment: const AlignmentDirectional(0, 0),
+        child: Padding(
+          padding: const EdgeInsetsDirectional.fromSTEB(20, 0, 20, 0),
+          child: TextFormField(
+            controller: emailController,
+            // onChanged: (_) => EasyDebounce.debounce(
+            //   'textController',
+            //   Duration(milliseconds: 2000),
+            //   () => setState(() {}),
+            // ),
+            autofocus: true,
+            obscureText: false,
+            decoration: InputDecoration(
+              labelText: 'Email Address',
+              enabledBorder: OutlineInputBorder(
+                // ignore: prefer_const_constructors
+                borderSide: BorderSide(
+                  // ignore: use_full_hex_values_for_flutter_colors
+                  color: const Color(0xFFBDBDBDBD),
+                  width: 1,
+                ),
+                borderRadius: BorderRadius.circular(8),
+              ),
+              focusedBorder: OutlineInputBorder(
+                borderSide: const BorderSide(
+                  // ignore: use_full_hex_values_for_flutter_colors
+                  color: Color(0xFFBDBDBDBD),
+                  width: 1,
+                ),
+                borderRadius: BorderRadius.circular(8),
+              ),
+            ),
+            style: const TextStyle(fontFamily: "Poppins"),
+          ),
+        ),
+      ),
+    );
+
+    //reset password button
+    final resetPasswordBtt = Padding(
+      padding: const EdgeInsetsDirectional.fromSTEB(0, 0, 0, 400),
+      child: ElevatedButton(
+        onPressed: () {},
+        child: const Text(
+          "Reset Password",
+          style: TextStyle(
+            fontFamily: 'Poppins',
+            fontSize: 14,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        style: ElevatedButton.styleFrom(
+          padding: const EdgeInsets.symmetric(horizontal: 50, vertical: 10),
+          primary: const Color(0xFF00e5FF),
+        ),
+      ),
+    );
+
     return Scaffold(
       key: scaffoldKey,
       appBar: PreferredSize(
@@ -77,81 +138,29 @@ class _ForgetPasswordPageWidgetState extends State<ForgetPasswordPageWidget> {
       body: SafeArea(
         child: GestureDetector(
           onTap: () => FocusScope.of(context).unfocus(),
-          child: Column(
-            mainAxisSize: MainAxisSize.max,
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              // ignore: prefer_const_constructors
-              Padding(
-                padding: const EdgeInsetsDirectional.fromSTEB(0, 120, 0, 10),
+          child: SingleChildScrollView(
+            child: Column(
+              mainAxisSize: MainAxisSize.max,
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
                 // ignore: prefer_const_constructors
-                child: Text(
-                  'Receive an email to reset password:',
-                  style: const TextStyle(
-                    fontFamily: 'Poppins',
-                    fontSize: 15,
-                  ),
-                ),
-              ),
-              Expanded(
-                child: Align(
-                  alignment: const AlignmentDirectional(0, 0),
-                  child: Padding(
-                    padding: const EdgeInsetsDirectional.fromSTEB(20, 0, 20, 0),
-                    child: TextFormField(
-                      controller: emailController,
-                      // onChanged: (_) => EasyDebounce.debounce(
-                      //   'textController',
-                      //   Duration(milliseconds: 2000),
-                      //   () => setState(() {}),
-                      // ),
-                      autofocus: true,
-                      obscureText: false,
-                      decoration: InputDecoration(
-                        labelText: 'Email Address',
-                        enabledBorder: OutlineInputBorder(
-                          // ignore: prefer_const_constructors
-                          borderSide: BorderSide(
-                            // ignore: use_full_hex_values_for_flutter_colors
-                            color: const Color(0xFFBDBDBDBD),
-                            width: 1,
-                          ),
-                          borderRadius: BorderRadius.circular(8),
-                        ),
-                        focusedBorder: OutlineInputBorder(
-                          borderSide: const BorderSide(
-                            // ignore: use_full_hex_values_for_flutter_colors
-                            color: Color(0xFFBDBDBDBD),
-                            width: 1,
-                          ),
-                          borderRadius: BorderRadius.circular(8),
-                        ),
-                      ),
-                      style: const TextStyle(fontFamily: "Poppins"),
-                    ),
-                  ),
-                ),
-              ),
-              Padding(
-                padding: const EdgeInsetsDirectional.fromSTEB(0, 0, 0, 400),
-                child: ElevatedButton(
-                  onPressed: () {},
-                  child: const Text(
-                    "Reset Password",
-                    style: TextStyle(
+                Padding(
+                  padding: const EdgeInsetsDirectional.fromSTEB(0, 120, 0, 10),
+                  // ignore: prefer_const_constructors
+                  child: Text(
+                    'Receive an email to reset password:',
+                    style: const TextStyle(
                       fontFamily: 'Poppins',
-                      fontSize: 14,
-                      fontWeight: FontWeight.bold,
+                      fontSize: 15,
                     ),
                   ),
-                  style: ElevatedButton.styleFrom(
-                    padding: const EdgeInsets.symmetric(
-                        horizontal: 50, vertical: 10),
-                    primary: const Color(0xFF00e5FF),
-                  ),
                 ),
-              ),
-            ],
+                //email address text field
+                emailAddress,
+                //reset password button
+                resetPasswordBtt,
+              ],
+            ),
           ),
         ),
       ),
